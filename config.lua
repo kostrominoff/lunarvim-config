db9b0733dce23317aba3a2c7174df05a3dc2fe2a
kostrominoff <zootikchannel@gmail.com>
2023-05-05 08:55:19 +0500

    Transparency

--- config.lua
+++ config.lua
@@ -20,13 +20,14 @@ lvim.keys.normal_mode["<leader>q"] = "<cmd>confirm q<cr>"
 lvim.keys.normal_mode["x"] = '"_x'
 
 lvim.colorscheme = "catppuccin"
-
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.treesitter.auto_install = true
+
+lvim.transparent_window = true
 
 vim.diagnostic.config({ update_in_insert = true })
 
@@ -39,7 +40,7 @@ lvim.builtin.alpha.dashboard.section.header.val = {
   "      ██╗  ██╗ ██████╗ ███████╗████████╗██████╗  ██████╗ ███╗   ███╗██╗███╗   ██╗",
   "      ██║ ██╔╝██╔═══██╗██╔════╝╚══██╔══╝██╔══██╗██╔═══██╗████╗ ████║██║████╗  ██║",
   "      █████╔╝ ██║   ██║███████╗   ██║   ██████╔╝██║   ██║██╔████╔██║██║██╔██╗ ██║",
-  "      ██╔═██╗ ██║   ██║╚════██║   ██║   ██╔══██╗██║   ██║██║╚██╔╝██║██║██║╚██╗██",
+  "      ██╔═██╗ ██║   ██║╚════██║   ██║   ██╔══██╗██║   ██║██║╚██╔╝██║██║██║╚██╗██║",
   "      ██║  ██╗╚██████╔╝███████║   ██║   ██║  ██║╚██████╔╝██║ ╚═╝ ██║██║██║ ╚████║",
   "      ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝",
   "",
